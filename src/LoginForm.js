4282108de25f88aaf89f7f1bcf1b2d411fb49239
Najeeb Idrees <najeebidrees@gmail.com>
2017-09-20 21:12:21 +0500

    Fix login fields
    Fix login fields

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -18,71 +18,52 @@ export default class LoginForm extends Component {
   render() {
     return (
 
-      <View style={styles.containerStyle}>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
-        <Text> TimeSheet App!! </Text>
+      <View
+        style={styles.loginLayoutStyle}
+      >
+        <Hideo
+          iconClass={FontAwesomeIcon}
+          iconName='envelope'
+          iconColor='white'
+          placeholder="Username or Email"
+          iconBackgroundColor={'#A8A099'}
+          inputStyle={{ color: '#464949' }}
+          value={this.state.username}
+          onChangeText={(text) => { this.setState({ username: text }) }}
+        />
+        <Hideo
+          style={styles.input}
+          iconClass={FontAwesomeIcon}
+          iconName='user'
+          iconColor='white'
+          placeholder="Password"
+          secureTextEntry
+          iconBackgroundColor={'#A8A099'}
+          inputStyle={{ color: '#464949' }}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }) }}
+        />
 
-
-        <View
-          style={styles.loginLayoutStyle}
+        <Button
+          onPress={this.onLoginPress.bind(this)}
+          btnStyle={styles.btnStyle}
         >
-          <Hideo
-            iconClass={FontAwesomeIcon}
-            iconName='envelope'
-            iconColor='white'
-            placeholder="Username or Email"
-            iconBackgroundColor={'#A8A099'}
-            inputStyle={{ color: '#464949' }}
-            value={this.state.username}
-            onChangeText={(text) => { this.setState({ username: text }) }}
-          />
-          <Hideo
-            style={styles.input}
-            iconClass={FontAwesomeIcon}
-            iconName='user'
-            iconColor='white'
-            placeholder="Password"
-            secureTextEntry
-            iconBackgroundColor={'#A8A099'}
-            inputStyle={{ color: '#464949' }}
-            value={this.state.password}
-            onChangeText={(text) => { this.setState({ password: text }) }}
-          />
-
-          <Button
-            onPress={this.onLoginPress.bind(this)}
-            btnStyle={styles.btnStyle}
-          >
-            Login
+          Login
         </Button>
 
-        </View>
       </View>
     );
   }
 }
 
 const styles = {
-  containerStyle: {
+  loginLayoutStyle: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#24BE9D',
     paddingLeft: 16,
     paddingRight: 16,
-  },
-  loginLayoutStyle: {
-    flexGrow: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    alignSelf: 'center',
-    left: 16,
-    right: 16,
   },
   input: {
     marginTop: 4,
